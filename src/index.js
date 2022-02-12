fd8c9240fcb89f9cdff0528d7562a51c0c72c6a6
Lucius <luciushehe@protonmail.com>
2022-02-11 18:30:37 -0600

    reset default browser styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Normalize } from 'styled-normalize';
 import App from './App';
 
+const Root = () => (
+  <>
+    <Normalize />
+    <App />
+  </>
+);
+
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
