5f69102a31a54561adf3a4862d360df91a405d45
Lucius <luciushehe@protonmail.com>
2022-02-11 18:50:23 -0600

    install and import fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Normalize } from "styled-normalize";
+import "@fontsource/bitter";
+import "@fontsource/montserrat";
 import App from "./App";
 
 const Root = () => (
