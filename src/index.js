7ff941ea1ba98cb7327475882dc36c3c666cb6ea
0xlucius <luciushehe@protonmail.com>
2022-02-14 13:02:29 -0500

    Create app skeleton (#1)
    * reset default browser styles
    
    * install and config prettier
    
    * install and import fonts
    
    * add global body styles
    
    * Add router with routes / and /search
    
    * setup routes folder
    
    * Add eslint prettier plugin
    
    * remove eslint react plugin
    
    * Install babel-eslint
    
    * remove eslint from package.json and reinstall dependencies to fix eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Normalize } from "styled-normalize";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import "@fontsource/bitter";
+import "@fontsource/montserrat";
+import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "montserrat", sans-serif;
+    font-size: 16px;
+    line-height: 20px;
+    color: #93918f;
+  }
+`;
+
+function Root() {
+  return (
+    <>
+      <GlobalStyle />
+      <Normalize />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="search" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
